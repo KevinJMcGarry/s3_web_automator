ccf8ed844fa3d282331717aaab3a537b530c5bc5
Kevin McGarry <kevinjmcgarry@gmail.com>
2018-08-12 20:18:27 -0500

    added in argument parsing and a couple of functions

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # AWS Automation - S3_Web_Automator
 
+## S3 Webinator
 This script will sync a local directory to an S3 bucket, and optionally configure Route53 and CloudFront as well.
+
+### Features
+The S3 Webinator currently has the following features:
+
+- List Buckets
+- List Contents of a Bucket
+
+example - python webinator.py list\_bucket\_objects --Bucket_Name someS3Bucket
+
---
+++ webinator/webinator.py
@@ -0,0 +1,45 @@
+import boto3
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Arguments for the S3 Boto3 Session')
+parser.add_argument('Command', help='Command can be "list_buckets", "list_bucket_objects"')
+parser.add_argument('--Bucket_Name', default='us', help='country zip/postal belongs to, default is "us"')
+
+# need to add some error handling for the commands
+
+args = parser.parse_args()
+
+if args.Bucket_Name:  # if the Bucket_Name optional argument is used
+    bucket_name = args.Bucket_Name
+
+
+session = boto3.Session(profile_name='eureka-terraform')
+s3 = session.resource('s3')
+
+def cli():
+    """S3 Webinator deploys websites to S3 buckets"""
+    pass
+
+
+def list_buckets():
+    """List all S3 Buckets"""
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+
+def list_bucket_objects(bucket):
+    """List Objects in an S3 Bucket"""
+    print(f"Objects in Bucket {bucket}")
+    print("-" * 35)
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+
+
+if args.Command == "list_buckets":
+    list_buckets()
+elif args.Command == "list_bucket_objects":
+    list_bucket_objects(bucket_name)
+
+
+#if __name__ == '__main__':
+    #list_buckets()
